8277afcc1a6ed67f01b9580519b2189c9030abba
Huy Phan <huyphan520@gmail.com>
2023-01-13 10:23:41 -0600

    IC Final Iteration 2

---
+++ lib/department.rb
@@ -0,0 +1,12 @@
+class Department
+  attr_reader :name, :employees
+
+  def initialize(name)
+    @name = name
+    @employees = []
+  end
+
+  def hire(employee)
+    @employees.push(employee)
+  end
+end
---
+++ spec/department_spec.rb
@@ -0,0 +1,46 @@
+require './lib/department'
+require './lib/employee'
+
+RSpec.describe Department do
+  describe "#initialize" do
+    it 'exists' do
+      customer_service = Department.new("Customer Service")
+
+      expect(customer_service).to be_instance_of(Department)
+    end
+
+    it 'has attributes' do
+      customer_service = Department.new("Customer Service")
+      
+      expect(customer_service.name).to eq("Customer Service")
+      expect(customer_service.employees).to eq([])
+    end
+  end
+
+  describe "#hire" do
+    it 'can hire employees' do
+      customer_service = Department.new("Customer Service")
+
+      bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "100000"})
+      aaron = Employee.new({name: "Aaron Tanaka", age: "25", salary: "90000"})  
+
+      customer_service.hire(bobbi)
+      customer_service.hire(aaron)    
+
+      expect(customer_service.employees).to eq([bobbi, aaron])
+    end
+  end
+
+  describe "#expenses" do
+    it 'can have expenses' do
+      customer_service = Department.new("Customer Service")
+
+      expect(customer_service.expenses).to eq(0)
+
+      customer_service.expense(100)
+      customer_service.expense(25)  
+
+      expect(customer_service.expenses).to eq(125)
+    end
+  end
+end
